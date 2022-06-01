62fffcd260b5e86f14d21265e93072f1265c6c39
Tampoka <goal.thanks@gmail.com>
2022-06-01 22:44:50 +0900

    Add loading instruments.

--- src/components/Keyboard/WithInstrument.tsx
+++ src/components/Keyboard/WithInstrument.tsx
@@ -1,14 +1,20 @@
 import {useAudioContext} from '../AudioContextProvider/useAudioContext';
 import {useSoundfont} from '../../adapters/Soundfont/useSoundFont';
 import {Keyboard} from './Keyboard';
-import {useMount} from '../../utils/useMount';
+import {useInstrument} from '../../state/Instrument/Context';
+import {useEffect} from 'react';
 
 export const KeyboardWithInstrument = () => {
-    const AudioContext=useAudioContext()!
-    const {loading, play,stop,load}=useSoundfont({AudioContext})
+    const AudioContext = useAudioContext()!
+    const {instrument} = useInstrument()
+    const {loading, current, play, stop, load} = useSoundfont({AudioContext})
 
-    useMount(load)
+    useEffect(() => {
+        if (!loading && instrument !== current) load(instrument)
+    }, [load, loading, current, instrument])
 
-    return <Keyboard loading={loading} play={play} stop={stop} />
+    console.log(instrument)
+    return <Keyboard loading={loading} play={play} stop={stop}/>
 };
 
+//todo fix rerender
