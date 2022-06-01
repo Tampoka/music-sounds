c7f8549611a044405de1455716e5a70525846c80
Tampoka <goal.thanks@gmail.com>
2022-06-01 20:37:04 +0900

    Add usePressObserver.

--- src/components/Key/Key.tsx
+++ src/components/Key/Key.tsx
@@ -2,6 +2,7 @@ import styles from './Key.module.css';
 import {NoteType} from '../../domain/note';
 import {FC} from 'react';
 import clsx from 'clsx';
+import {usePressObserver} from '../PressObserver';
 
 type PressCallback = () => void
 type KeyProps = {
@@ -13,9 +14,15 @@ type KeyProps = {
 }
 export const Key: FC<KeyProps> = (props) => {
     const {type, label, onUp, onDown, ...rest} = props
+    const pressed = usePressObserver({
+        watchKey: label,
+        onStartPress: onDown,
+        onFinishPress: onUp
+    })
     return (
         <button
-            className={clsx(styles.key, styles[type])}
+            className={clsx(styles.key, styles[type],
+                pressed && "is-pressed")}
             onMouseDown={onDown}
             onMouseUp={onUp}
             type="button"
