6eb51a04bc004001f8c5ae589941d99ceeba7244
Tampoka <goal.thanks@gmail.com>
2022-06-01 16:03:43 +0900

    Fix onUp,onDown props.

--- src/components/Keyboard/Keyboard.tsx
+++ src/components/Keyboard/Keyboard.tsx
@@ -1,14 +1,30 @@
 import styles from './Keyboard.module.css'
-import {notes} from '../../domain/note';
+import {MidiValue, notes} from '../../domain/note';
 import {selectKey} from '../../domain/keyboard';
 import {Key} from '../Key';
+import {FC} from 'react';
 
-export const Keyboard = () => {
+export type KeyboardProps = {
+    loading: boolean
+    play: (note: MidiValue) => Promise<void>
+    stop: (note: MidiValue) => Promise<void>
+}
+
+export const Keyboard: FC<KeyboardProps> = ({loading, play, stop}) => {
     return (
         <div className={styles.keyboard}>
             {notes.map(({midi, type, index, octave}) => {
                 const label = selectKey(octave, index)
-                return <Key key={midi} type={type} label={label}/>
+                return(
+                    <Key
+                    key={midi}
+                    type={type}
+                    label={label}
+                    disabled={loading}
+                    onDown={() => play(midi)}
+                    onUp={() => stop(midi)}
+                />
+                )
             })}
         </div>
     );
